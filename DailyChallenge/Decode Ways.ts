0a766f1dd65546010eb990cfafeafd8332d33fa2
kanpech-t <kankasem3002@gmail.com>
2023-12-25 22:01:07 +0700

    dailycode

---
+++ DailyChallenge/Decode Ways.ts
@@ -0,0 +1,21 @@
+const numDecodings = (s: string): number => {
+  const helper = (index: number): number => {
+    if (index === s.length) {
+      return 1;
+    }
+
+    if (s[index] === "0") {
+      return 0;
+    }
+
+    let ways = helper(index + 1);
+    console.log(ways);
+
+    if (index < s.length - 1 && parseInt(s.substring(index, index + 2)) <= 26) {
+      ways += helper(index + 2);
+    }
+    return ways;
+  };
+
+  return helper(0);
+};
